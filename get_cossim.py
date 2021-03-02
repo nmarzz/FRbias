7cfe1af993a458577da9a0d07795def09d6a92c2
nmarzz <ndmarshall97@gmail.com>
2021-03-02 10:57:21 -0800

    save sphere face embeds

--- get_cossim.py
+++ get_cossim.py
@@ -141,8 +141,7 @@ for ethnic in ethnicities:
                             embedding1 = torch.linalg.norm(embedding1[1],dim = (2,3))
                             embedding2 = torch.linalg.norm(embedding2[1],dim = (2,3))
 
-                    if args.save_embed:
-                        print(embedding1)
+                    if args.save_embed:                        
                         embedding_dict[path1] = embedding1.cpu()
                         embedding_dict[path2] = embedding2.cpu()
 
